d357016088fcc754fef84fb270e5eb9679ffca0b
marcos <marcoskilf@gmail.com>
2016-06-19 20:44:36 -0300

    Adicionado sript sql

---
+++ BD/Criação de tabelas.sql
@@ -0,0 +1,22 @@
+-- Table: usuario
+
+-- DROP TABLE usuario;
+
+CREATE TABLE usuario
+(
+  usu_nm character varying,
+  usu_id serial NOT NULL,
+  usu_email character varying,
+  usu_senha character varying,
+  usu_nivel_acesso character varying,
+  CONSTRAINT pk_usuario_usu_id PRIMARY KEY (usu_id),
+  CONSTRAINT uk_usuario_usu_nm UNIQUE (usu_nm)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE usuario
+  OWNER TO postgres;
+
+  insert into usuario(usu_nm, usu_email, usu_senha, usu_nivel_acesso)
+  	values ('Marcos', 'marcos@gmail.com', '12345', 'ROLE_ADMINISTRADOR');
